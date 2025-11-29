9936402a634941aa0c3c3fb1b02b174834baf846
Arun Kumar <sivaarun10@gmail.com>
2025-11-29 18:29:36 +0530

    uploades

--- Aryu/aryuapp/routing.py
+++ Aryu/aryuapp/routing.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_id>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_id>[0-9]+)/?$", consumers.ChatConsumer.as_asgi()),
     re_path(r"ws/notifications/$", consumers.NotificationConsumer.as_asgi()),
 ]
 
